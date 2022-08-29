bb69e52e465ca7d4ec289ba9b4060192b5a426c2
shuhongfan <shfshuhongfan@gmail.com>
2022-08-29 11:27:13 +0800

    升级 七牛云OSS

--- yami-shop-admin/src/main/java/com/yami/shop/admin/controller/FileController.java
+++ yami-shop-admin/src/main/java/com/yami/shop/admin/controller/FileController.java
@@ -14,10 +14,7 @@ import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.yami.shop.bean.dto.TinymceEditorDto;
@@ -32,12 +29,12 @@ import com.yami.shop.service.AttachFileService;
 @RestController
 @RequestMapping("/admin/file")
 public class FileController {
-	
+
 	@Autowired
 	private AttachFileService attachFileService;
 	@Autowired
 	private Qiniu qiniu;
-	
+
 	@PostMapping("/upload/element")
 	public ResponseEntity<String> uploadElementFile(@RequestParam("file") MultipartFile file) throws IOException{
 		if(file.isEmpty()){
@@ -46,11 +43,11 @@ public class FileController {
 		String fileName = attachFileService.uploadFile(file.getBytes(),file.getOriginalFilename());
         return ResponseEntity.ok(fileName);
 	}
-	
+
 	@PostMapping("/upload/tinymceEditor")
 	public ResponseEntity<String> uploadTinymceEditorImages(@RequestParam("editorFile") MultipartFile editorFile) throws IOException{
 		String fileName =  attachFileService.uploadFile(editorFile.getBytes(),editorFile.getOriginalFilename());
         return ResponseEntity.ok(qiniu.getResourcesUrl() + fileName);
 	}
-	
+
 }
